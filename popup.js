ef3dc475314d4f1d78b4c60805ae8777ff2d936e
Code Weft <codeweft@Codes-MacBook-Pro.local>
2014-10-14 15:27:33 +0800

    blocking google urls

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
+//var items = {
+//  "foo": 123456,
+//  "bar": 789012,
+//  "baz": 345678,
+//  "bat": 901234
+//};
 
+//
+//for(var index in items) {
+//  document.write( index + " : " + items[index] + "<br />");
+//}
+
+function checkUrl(url) {
+  return url.match(/.*google.*/);
+//  return true
+//
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      return {cancel: details.url.indexOf("://fonts.gstatic.com/") != -1};
+    function (details) {
+      return {cancel: checkUrl(details.url) != null};
     },
     {urls: ["<all_urls>"]},
     ["blocking"]);
