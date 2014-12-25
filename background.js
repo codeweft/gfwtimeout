d0ce797bcb3d655dbd51dc05cf8ca8b2d62af704
codeweft <codeweft@gmail.com>
2014-12-25 16:25:29 +0800

    updating  javascripts

--- background.js
+++ background.js
@@ -1,75 +1,66 @@
-function modifyRequestHeaders(details, headers) {
-  if (!headers) {
-    return;
-  }
-  for (var i = 0, length = headers.length; i < length; ++i) {
-    var currHeader = headers[i];
-    // Overrides the header if it is enabled and its name is not empty.
-    if (currHeader.enabled && currHeader.name) {
-      details.requestHeaders.push({name: currHeader.name, value: currHeader.value});
-    }
-  }
-};
 
-/**
- * Check whether the current request url pass the given list of filters.
- */
-function passFilters(url, filters) {
-  if (!filters) {
-    return true;
-  }
-  var allow = false;
-  for (var i = 0, length = filters.length; i < length; ++i) {
-    var host = filters[i].host;
-    if (!host) {
-      allow = true;
-      continue;
-    }
-    if (url.search(host.replace('*', '.*')) == 0) {
-      allow = filters[i].behavior;
-    }
-  }
-  return String(allow) == "true";
-};
 
-/**
- * Gets the currently selected profile. Maybe null.
- */
-function getCurrentProfile() {
-  if (localStorage.profiles) {
-    var profiles = $.parseJSON(localStorage.profiles);
-    if (!localStorage.selectedProfile) {
-      localStorage.selectedProfile = 0;
-    }
-    return profiles[localStorage.selectedProfile];
-  }
-};
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    var currProfile = getCurrentProfile();
-    if (currProfile) {
-      if (passFilters(details.url, currProfile.filters)) {
-        modifyRequestHeaders(details, currProfile.headers);
-      }
-    }
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: []},
-  ['requestHeaders', 'blocking']
-);
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.active && tab.highlighted) {
-    if (tab.url) {
-      var currProfile = getCurrentProfile();
-      if (currProfile) {
-        var icon = 'icon.png';
-        if (!passFilters(tab.url, currProfile.filters)) {
-          icon = 'icon_bw.png';
+function checkUrl() {
+    var count = 0;
+    var string2 = 'a';
+    todoitems = getItem("todoitems")
+    var regex = new RegExp( string2, 'g' );
+    angular.forEach(todoitems, function(todo) {
+        if(todo.done) {
+            if (url.match("/.*"+ todo.text +".*/")){
+                count = count + 1
+            }
         }
-        chrome.browserAction.setIcon({path: icon, tabId: tabId});
-      }
+    });
+    if (count > 0) return true;
+    return false;
+};
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        return {cancel: checkUrl(details.url) != false};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]);
+
+logging = true
+function setItem(key, value) {
+    try {
+        log("Storing [" + key + ":" + value + "]");
+        window.localStorage.removeItem(key);      // <-- Local storage!
+        window.localStorage.setItem(key, value);  // <-- Local storage!
+    } catch(e) {
+        log("Error inside setItem");
+        log(e);
     }
-  }
-});
+    log("Return from setItem" + key + ":" +  value);
+}
+
+// Gets item from local storage with specified key.
+function getItem(key) {
+    var value;
+    log('Retrieving key [' + key + ']');
+    try {
+        value = window.localStorage.getItem(key);  // <-- Local storage!
+    }catch(e) {
+        log("Error inside getItem() for key:" + key);
+        log(e);
+        value = "null";
+    }
+    log("Returning value: " + value);
+    return value;
+}
+
+// Clears all key/value pairs in local storage.
+function clearStrg() {
+    log('about to clear local storage');
+    window.localStorage.clear(); // <-- Local storage!
+    log('cleared');
+}
+
+function log(txt) {
+    if(logging) {
+        console.log(txt);
+    }
+}
+
+
--- opts.js
+++ opts.js
@@ -7,6 +7,7 @@ angular.module('todoApp', [])
         $scope.addTodo = function() {
             $scope.todos.push({text:$scope.todoText, done:false});
             $scope.todoText = '';
+            setItem("todoitems", $scope.todos)
         };
 
         $scope.remaining = function() {
@@ -24,28 +25,47 @@ angular.module('todoApp', [])
                 if (!todo.done) $scope.todos.push(todo);
             });
         };
-
-        $scope.checkUrl = function() {
-            var count = 0;
-            var string2 = 'a';
-            var regex = new RegExp( string2, 'g' );
-            angular.forEach($scope.todos, function(todo) {
-                if(todo.done) {
-                    if (url.match("/.*"+ todo.text +".*/")){
-                        count = count + 1
-                    }
-                }
-            });
-            if (count > 0) return true;
-            return false;
-        };
     }]);
 
+logging=true;
+function setItem(key, value) {
+    try {
+        log("Storing [" + key + ":" + value + "]");
+        window.localStorage.removeItem(key);      // <-- Local storage!
+        window.localStorage.setItem(key, value);  // <-- Local storage!
+    } catch(e) {
+        log("Error inside setItem");
+        log(e);
+    }
+    log("Return from setItem" + key + ":" +  value);
+}
+
+// Gets item from local storage with specified key.
+function getItem(key) {
+    var value;
+    log('Retrieving key [' + key + ']');
+    try {
+        value = window.localStorage.getItem(key);  // <-- Local storage!
+    }catch(e) {
+        log("Error inside getItem() for key:" + key);
+        log(e);
+        value = "null";
+    }
+    log("Returning value: " + value);
+    return value;
+}
+
+// Clears all key/value pairs in local storage.
+function clearStrg() {
+    log('about to clear local storage');
+    window.localStorage.clear(); // <-- Local storage!
+    log('cleared');
+}
+
+function log(txt) {
+    if(logging) {
+        console.log(txt);
+    }
+}
 
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        return {cancel: checkUrl(details.url) != null};
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]);
